8ca85f8e6d33f63b4f50269e25bd805a050ad18e
chris rollins <chris.rollins@leapfire.com>
2018-08-30 11:55:41 -0700

    switched to .NET core

--- Alabaster/API/Client.cs
+++ Alabaster/API/Client.cs
@@ -10,19 +10,18 @@ namespace Alabaster
 {
     public static class Client
     {
-        private static HttpClient client = new HttpClient();
-        
-        public enum Scheme : byte { HTTP, HTTPS };
+        private static HttpClient client = new HttpClient();   
 
-        public static async Task<string> Get(string url, Scheme scheme = Scheme.HTTP) => await Request("GET", url, "", scheme);
-        public static async Task<string> Delete(string url, Scheme scheme = Scheme.HTTP) => await Request("DELETE", url, "", scheme);
-        public static async Task<string> Post(string url, string body, Scheme scheme = Scheme.HTTP) => await Request("POST", url, body, scheme);
-        public static async Task<string> Patch(string url, string body, Scheme scheme = Scheme.HTTP) => await Request("PATCH", url, body, scheme);
-        public static async Task<string> Put(string url, string body, Scheme scheme = Scheme.HTTP) => await Request("PUT", url, body, scheme);
+        public static async Task<string> Get(string url, HTTPScheme scheme = HTTPScheme.HTTP) => await Request("GET", url, "", scheme);
+        public static async Task<string> Delete(string url, HTTPScheme scheme = HTTPScheme.HTTP) => await Request("DELETE", url, "", scheme);
+        public static async Task<string> Post(string url, string body, HTTPScheme scheme = HTTPScheme.HTTP) => await Request("POST", url, body, scheme);
+        public static async Task<string> Patch(string url, string body, HTTPScheme scheme = HTTPScheme.HTTP) => await Request("PATCH", url, body, scheme);
+        public static async Task<string> Put(string url, string body, HTTPScheme scheme = HTTPScheme.HTTP) => await Request("PUT", url, body, scheme);
 
-        public static async Task<string> Request(string method, string url, string body, Scheme scheme = Scheme.HTTP)
+        public static async Task<string> Request(string method, string url, string body, HTTPScheme scheme = HTTPScheme.HTTP)
         {
-            if(url.Substring(0, 4).ToUpper() == "HTTP") { throw new ArgumentException("HTTP scheme must not be defined in the URL."); }            
+            if (scheme != HTTPScheme.HTTP && scheme != HTTPScheme.HTTPS) { throw new ArgumentException("HTTP scheme must be HTTP or HTTPS."); }
+            if (url.Substring(0, 4).ToUpper() == "HTTP") { throw new ArgumentException("HTTP scheme must not be defined in the URL."); }            
             string fullURL = String.Join(null, scheme.ToString().ToLower() , "://" , url);
             using (HttpRequestMessage msg = new HttpRequestMessage(new HttpMethod(method), fullURL))
             {
